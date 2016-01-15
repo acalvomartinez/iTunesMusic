a5d3be3f4406aa38f2652373e3bb61cdcd027b11
Toni <toniconmac@gmail.com>
2016-01-15 11:55:27 +0100

    add gcd refactoring

--- iTunesMusic/IMArtistListDataManager.m
+++ iTunesMusic/IMArtistListDataManager.m
@@ -33,19 +33,22 @@
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0);
     dispatch_async(queue, ^{
         [self fetchFromPesistenceStoreOnCompletionBlock:^(NSArray *artists) {
-            if (completionBlock) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (completionBlock) {
                 completionBlock(artists);
             }
+            });
         }];
     });
     dispatch_async(queue, ^{
         [self fecthFromServiceOnCompletionBlock:^(NSArray *artists) {
-            if (completionBlock) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (completionBlock) {
                 completionBlock(artists);
             }
+            });
         }];
     });
-  
 }
 
 - (void)fetchFromPesistenceStoreOnCompletionBlock:(IMArtistListDataManagerFetchArtistsBlock)completionBlock {
--- iTunesMusic/IMArtistListInteractor.m
+++ iTunesMusic/IMArtistListInteractor.m
@@ -30,16 +30,12 @@
 }
 
 - (void)findArtists {
-    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0);
-    dispatch_async(queue, ^{
-        __weak typeof(self) weakSelf = self;
-        [self.dataManager fetchArtistsOnCompletionBlock:^(NSArray *artists) {
-            NSArray *artistList = [weakSelf artistsFromModelArtists:artists];
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [weakSelf.output foundArtists:artistList];
-            });
-        }];
-    });
+    __weak typeof(self) weakSelf = self;
+    [self.dataManager fetchArtistsOnCompletionBlock:^(NSArray *artists) {
+        NSArray *artistList = [weakSelf artistsFromModelArtists:artists];
+        [weakSelf.output foundArtists:artistList];
+        
+    }];
 }
 
 - (NSArray *)artistsFromModelArtists:(NSArray *)modelArtists {
